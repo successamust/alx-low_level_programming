03b36a540f352672b8a7289c011583556cbcf489
Salami Fatai <fataiopeyemi1@gmail.com>
2022-09-08 09:00:18 -0700

    puts commamd

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with puts function
+ *
+ * Return: always 0 when successful.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
