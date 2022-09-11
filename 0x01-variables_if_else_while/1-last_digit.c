887f4ff4ec2c0c30d65299deb8b5736961fa13f3
Salami Fatai <fataiopeyemi1@gmail.com>
2022-09-11 16:59:33 -0700

    Using if and else of statements

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * Main - Entry point
+ *
+ * Description - Print the value of last digit of n: greater than 5, eqaul to zero, and not less than 6
+ *
+ * Retun - (0) if successful 
+ */
+
+int main(void)
+{
+	int n, LD; //LD = Last digit
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	LD = n % 10; //this get last digit
+	if (LD > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, LD);
+	else if (LD == 0)
+		printf("Last digit of %i is %i and is 0\n", n, LD);
+	else if (LD < 6 && LD != 0)
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, LD);
+
+	return (0);
+}
