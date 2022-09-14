78d0fe27f5da3755fc92f9d169307be09983a02b
Salami Fatai <fataiopeyemi1@gmail.com>
2022-09-14 13:41:45 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+
+ #include"main.h"
+
+/**
+ * printing alphabets unsing putchar function
+ *
+ * return a-z 
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= z; ++ch)
+		_putchar(ch);
+	_putchar ('\n');
+}
