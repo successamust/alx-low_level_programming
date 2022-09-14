209f9ce89108b562392265946173b88f1aed026c
Salami Fatai <fataiopeyemi1@gmail.com>
2022-09-14 14:52:27 -0700

    islower function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/** 
+ * _islower - function for checking lower case character.
+ * @c: checks input of function
+ * return 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
+
