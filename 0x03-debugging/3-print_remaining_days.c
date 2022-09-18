57db9de79d40bf583ddecafada826846718e3d19
Salami Fatai <fataiopeyemi1@gmail.com>
2022-09-18 07:15:16 -0700

    debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+
 /**print_remaining_days - takes a date and prints how many days are
  * print_remaining_days - takes a date and prints how many days are
  * @month: month in number format
@@ -7,6 +8,7 @@
  * @year: year
  * Return: void
  */
+
 void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
