a3f4d8c1058c7a3eb238dcd7dec3a7f8c3c8d9d9
Salami Fatai <fataiopeyemi1@gmail.com>
2022-09-18 10:04:57 -0700

    nested loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,7 +14,7 @@ void print_square(int size)
 
 	for (row = 1; row <= size; row++)
 	{
-		for (column = 0; column <= size; column++)
+		for (column = 1; column <= size; column++)
 			_putchar('#');
 		_putchar('\n');
 	}
