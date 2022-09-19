d706fedcb11d5fc62abefb2611379153d8c29a1b
Salami Fatai <fataiopeyemi1@gmail.com>
2022-09-19 15:09:03 -0700

    strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,7 +19,7 @@ void puts_half(char *str)
 	{
 		for (n = l / 2; str[n] != '\0'; ++n)
 			_putchar(str[n]);
-	}else
+	} else
 	{
 		for (n = ((l - 1) / 2) + 1; str[n] != '\0'; ++n)
 			putchar(str[n]);
