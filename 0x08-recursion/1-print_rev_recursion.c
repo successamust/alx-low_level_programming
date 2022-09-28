a1d18dcd06f9a66c1b40c1bf2151afe12643ed84
Salami Fatai <fataiopeyemi1@gmail.com>
2022-09-28 10:36:38 -0700

    reverse recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - a function that print a strin in reverse
+ * @s - string
+ * return 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
