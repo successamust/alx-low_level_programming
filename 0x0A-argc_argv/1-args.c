f2de3da7a88c6239cf133312c5ee573471c5a439
Salami Fatai <fataiopeyemi1@gmail.com>
2022-09-30 04:39:36 -0700

    prints argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - funtion that prints the number arguments passed into it
+ * @argc: hoold the count of arguments
+ * @argv: holds the array of argument passed
+ *
+ * Return 0
+ */
+
+int main(int argc, char* argv [])
+{
+	printf("argc = %d\n", argc);
+	return 0;
+
+}
