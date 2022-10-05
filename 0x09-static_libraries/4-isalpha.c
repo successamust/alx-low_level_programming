2fc80c705e277a8022f4c03c3a296f5b0fcbf245
Salami Fatai <fataiopeyemi1@gmail.com>
2022-10-05 13:00:13 -0700

    creating a static library

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * _isalpha - checks if character is a letter
+ * @c: takes input from other functions.
+ *
+ * Return: 1 is c if true else 0
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+	return (1);
+	}
+	else
+	return (0);
+}
+
